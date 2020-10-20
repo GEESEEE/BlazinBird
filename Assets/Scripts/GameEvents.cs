549109d15569d6dacbce51b3b8ea8bc1b65e9d0d
naimeds10 <giesdb@gmail.com>
2020-10-20 02:05:49 +0200

    Almost finished
    TODO: Fix volume slider

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+    // Start is called before the first frame update
+    private void Awake() {
+        current = this;
+    }
+
+    public event Action startGame;
+    public void StartGame()
+    {
+        if (startGame != null) 
+        {
+            startGame();
+        }
+    }
+
+    public event Action jump;
+    public void Jump()
+    {
+        if (jump != null)
+        {
+            jump();
+        }
+    }
+
+    public event Action passedObstacle;
+    public void PassedObstacle()
+    {
+        if (passedObstacle != null)
+        {
+            passedObstacle();
+        }
+    }
+
+
+
+    // Update is called once per frame
+    public event Action onObstacleHit;
+    public void ObstacleHit()
+    {
+        if (onObstacleHit != null)
+        {
+            onObstacleHit();
+        }
+    }
+
+}
---
+++ Assets/Scripts/JonkoParticles.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class JonkoParticles : MonoBehaviour
+{
+
+    public ParticleSystem smoke;
+    public ParticleSystem fire;
+    public ParticleSystem perpetualSmoke;
+    public int smokeParticleCount = 5;
+    public int fireParticleCount = 3;
+
+    public Transform particleSystemTransform;
+    public Transform jonkoTransform;
+    public float jonkoPofDelay = 1f;
+    public float OffsetX = 0.5f;
+    public float OffsetY = -0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Game") 
+        {
+            GameEvents.current.jump += MakeSmoke;
+            GameEvents.current.jump += MakeFire;
+            GameEvents.current.onObstacleHit += PerpetualSmoke;
+        }
+        
+        StartCoroutine(smokeBeforeGameStarts());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float angle = -jonkoTransform.eulerAngles.z * Mathf.PI / 180;
+        float finalOffsetX = OffsetX * Mathf.Cos(angle) + OffsetY * Mathf.Sin(angle);
+        float finalOffsetY = -OffsetX * Mathf.Sin(angle) + OffsetY * Mathf.Cos(angle);
+
+        particleSystemTransform.position = jonkoTransform.position + new Vector3(finalOffsetX, finalOffsetY, 0f);
+       
+    }
+
+    IEnumerator smokeBeforeGameStarts()
+    {
+        yield return new WaitForSeconds(1);
+        while (!GameManager.instance.gameHasStarted)
+        {
+            MakeSmoke();
+            MakeFire();
+            yield return new WaitForSeconds(jonkoPofDelay);
+        }
+    }
+
+    private void MakeSmoke()
+    {
+        var emitParams = new ParticleSystem.EmitParams();
+        smoke.Emit(emitParams, smokeParticleCount); 
+    }
+
+    private void MakeFire()
+    {
+        var emitParams = new ParticleSystem.EmitParams();
+        fire.Emit(emitParams, fireParticleCount);
+    }
+
+    private void PerpetualSmoke()
+    {
+        Destroy(fire);
+        Destroy(smoke);
+        perpetualSmoke.Play();
+    }
+
+    void OnDestroy()
+    {
+        if (SceneManager.GetActiveScene().name == "Game") 
+        {
+            GameEvents.current.jump -= MakeSmoke;
+            GameEvents.current.jump -= MakeFire;
+            GameEvents.current.onObstacleHit -= PerpetualSmoke;
+
+        }
+
+    }
+}
